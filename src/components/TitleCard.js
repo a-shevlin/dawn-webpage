6074f5fe07b86f2324530e92db36f271fcaa547f
Alex Shevlin <alexshevlin1@gmail.com>
2022-11-16 01:34:11 -0800

    finish fade on scroll.

--- src/components/TitleCard.js
+++ src/components/TitleCard.js
@@ -1,24 +1,32 @@
 import React, { useState, useEffect } from 'react';
+import dawnLogo from '../assets/logo_tra.png';
 
 function TitleCard() {
   const [currentScrollHeight, setCurrentScrollHeight] = useState(1);
-
+  const [show, setShow] = useState(true);
+  
   useEffect(() => {      
     window.onscroll =()=>{
-      const newScrollHeight = Math.ceil(window.scrollY / 50) *50;
-      if (currentScrollHeight !== newScrollHeight){
-        setCurrentScrollHeight(newScrollHeight);
+      console.log(window.scrollY);
+      // might need to change based on screen size
+      if (window.scrollY > 70){
+        setShow(false);
+      } else {
+        setShow(true);
       }
     }
   }, []);
   
-  const opacity = Math.min(30 / currentScrollHeight, 1);
-  console.log(opacity);
 
 
   return (
-    <div className='h-full flex justify-center items-center bg-fixed' style={{opacity}}>
-      <h1 className="fixed text-red-300" >Dawn</h1>
+    <div className='w-full flex flex-col justify-center bg-fixed sticky bg-base-panel bg-no-repeat bg-cover h-2/5'>
+      <div className={`text-red-100 font-mono fixed mx-auto w-full my-auto titleCardFade tracking-narrow drop-shadow-lg font-bold p-0 text-center uppercase justify-center ${!show && 'opacity-0'}`}>
+        <img className="w-36 block mx-auto my-0" src={dawnLogo} alt="dawn logo" />
+        <h1 className="text-8xl text-white mt-0">DAWN</h1>
+        <p className="text-2xl">we are designers for the dawn of your creation</p>
+        <p className="text-xl border-double border-4 border-white rounded-md w-fit mx-auto p-1">mc.dawnserver.com</p>
+      </div>
     </div>
   )
 }
