d9faf172215c81e72002f0efe8e07bbac109c439
a-shev <alexshevlin1@gmail.com>
2022-12-23 15:27:11 -0800

    add seed data for pages.

---
+++ src/data/pages.js
@@ -0,0 +1,32 @@
+const pages = [
+  {
+    name: "home",
+    id: 1,
+  },
+  {
+    name: "about us",
+    id: 2
+  },
+  {
+    name: "services",
+    id: 3
+  },
+  {
+    name: "news",
+    id: 4
+  },
+  {
+    name: "discord",
+    id: 5
+  },
+  {
+    name: "store",
+    id: 6
+  },
+  {
+    name: "marketplace",
+    id: 7
+  },
+]
+
+export default pages;
