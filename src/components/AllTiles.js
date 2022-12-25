9287ba1a99ef4a1f055efb11e2aabd21c61ab951
a-shev <alexshevlin1@gmail.com>
2022-12-24 18:47:46 -0800

    update image seed data to include name of build and builder.

--- src/components/AllTiles.js
+++ src/components/AllTiles.js
@@ -6,42 +6,62 @@ import bg from '../assets/dawn-bg.png';
 const data = [
   {
     id: 0,
+    name: 'tree',
+    builder: 'everyone',
     img: bg,
   },
   {
     id: 1,
+    name: 'tree',
+    builder: 'everyone',
     img: bg,
   },
   {
     id: 2,
+    name: 'tree',
+    builder: 'everyone',
     img: bg,
   },
   {
     id: 3,
+    name: 'tree',
+    builder: 'everyone',
     img: bg,
   },
   {
     id: 4,
+    name: 'tree',
+    builder: 'everyone',
     img: bg,
   },
   {
     id: 5,
+    name: 'tree',
+    builder: 'everyone',
     img: bg,
   },
   {
     id: 6,
+    name: 'tree',
+    builder: 'everyone',
     img: bg,
   },
   {
     id: 7,
+    name: 'tree',
+    builder: 'everyone',
     img: bg,
   },
   {
     id: 8,
+    name: 'tree',
+    builder: 'everyone',
     img: bg,
   },
   {
     id: 9,
+    name: 'tree',
+    builder: 'everyone',
     img: bg,
   }
 ]
@@ -49,12 +69,14 @@ const data = [
 function AllTiles() {
   return(
     <React.Fragment>
-      <div className='w-full h-full flex flex-col bg-zinc-200 dark:bg-zinc-700 p-1 z-10'>
+      <div className='w-full h-full flex flex-col bg-zinc-200 dark:bg-zinc-750 p-1'>
         <div className='text-zinc-500 font-mono mx-auto w-full titleCardFade tracking-narrow drop-shadow-lg font-bold p-0 text-center uppercase grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4'>
 
           {Object.values(data).map((tile) =>(
             <Tiles
               img={tile.img}
+              name={tile.name}
+              builder={tile.builder}
               id={tile.id}
             />
             ))}
