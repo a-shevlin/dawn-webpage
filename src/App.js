d1bcf58dc02866e7e70aff07c394529c76d40e38
a-shev <alexshevlin1@gmail.com>
2022-12-29 15:51:42 -0800

    update route paths.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TitleCard, Home, PageLinks } from './components';
+import { TitleCard, Home, PageLinks, About } from './components';
 import {
   BrowserRouter as Router,
   Routes,
@@ -10,12 +10,13 @@ import {
 
 function App() {
   return (
-      <div className="h-screen bg-zinc-200 dark:bg-zinc-700">
+      <div className="h-screen bg-zinc-200 dark:bg-slate-800">
     <Router>
         <TitleCard />
         <PageLinks />
         <Routes>
           <Route exact path='/' element={<Home />} />
+          <Route path='/About' element={<About />} />
           {/* <Route path='/about' element={<AboutUs />} /> */}
         </Routes>
         {/* <div className="flex-1 w-full z-10 h-full">
