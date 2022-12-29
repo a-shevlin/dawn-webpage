b5ca819be51cee8a9e42236ab0a34a3eaf361db1
a-shev <alexshevlin1@gmail.com>
2022-12-29 15:52:12 -0800

    update AllTiles to pull from data.

--- src/components/AllTiles.js
+++ src/components/AllTiles.js
@@ -1,78 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Tiles } from "./";
-import bg from '../assets/dawn-bg.png';
+import { tiles } from "../data"; 
 
-const data = [
-  {
-    id: 0,
-    name: 'tree',
-    builder: 'everyone',
-    img: bg,
-  },
-  {
-    id: 1,
-    name: 'tree',
-    builder: 'everyone',
-    img: bg,
-  },
-  {
-    id: 2,
-    name: 'tree',
-    builder: 'everyone',
-    img: bg,
-  },
-  {
-    id: 3,
-    name: 'tree',
-    builder: 'everyone',
-    img: bg,
-  },
-  {
-    id: 4,
-    name: 'tree',
-    builder: 'everyone',
-    img: bg,
-  },
-  {
-    id: 5,
-    name: 'tree',
-    builder: 'everyone',
-    img: bg,
-  },
-  {
-    id: 6,
-    name: 'tree',
-    builder: 'everyone',
-    img: bg,
-  },
-  {
-    id: 7,
-    name: 'tree',
-    builder: 'everyone',
-    img: bg,
-  },
-  {
-    id: 8,
-    name: 'tree',
-    builder: 'everyone',
-    img: bg,
-  },
-  {
-    id: 9,
-    name: 'tree',
-    builder: 'everyone',
-    img: bg,
-  }
-]
 
 function AllTiles() {
   return(
     <React.Fragment>
-      <div className='w-full h-full flex flex-col bg-zinc-200 dark:bg-zinc-750 p-1'>
-        <div className='text-zinc-500 font-mono mx-auto w-full titleCardFade tracking-narrow drop-shadow-lg font-bold p-0 text-center uppercase grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4'>
+      <div className='w-full h-full flex flex-col bg-zinc-200 dark:bg-slate-800 p-1'>
+        <div className='text-zinc-500 font-mono mx-auto w-full tracking-narrow drop-shadow-lg font-bold p-0 text-center uppercase grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4'>
 
-          {Object.values(data).map((tile) =>(
+          {Object.values(tiles).map((tile) =>(
             <Tiles
               img={tile.img}
               name={tile.name}
