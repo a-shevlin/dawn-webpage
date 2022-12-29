0a6b95bc03ff5d43e51a91f1c965bd592dc51e9c
a-shev <alexshevlin1@gmail.com>
2022-12-29 15:53:08 -0800

    update colors for dark mode.

--- src/components/PageLinks.js
+++ src/components/PageLinks.js
@@ -12,16 +12,15 @@ function PageLinks() {
   }, []);
   return(
     <React.Fragment>
-      <nav className="bg-zinc-200 dark:bg-zinc-750 text-center flex flex-wrap space-x-3 w-full justify-center pt-5 pb-4 z-50">
+      <nav className="bg-zinc-200 dark:bg-slate-800 text-center flex flex-wrap space-x-3 w-full justify-center pt-5 pb-4 z-50">
     { !width ?
       <div>
 
         {Object.values(pages).map((page) => (
-          <div className="inline-block lg:blocks p-2 px-5 text-zinc-600 hover:text-red-500 hover:drop-shadow-md dark:text-zinc-100 dark:hover:text-red-300 hover:cursor-pointer uppercase scale-y-90 tracking-wide font-thin text-xl hover:dark:bg-zinc-700 hover:bg-zinc-300 rounded-lg">
-            {/* <Link to={`/${page.name}`}> */}
-
+          <div className="inline-block lg:blocks p-2 px-5 text-zinc-600 hover:text-red-500 hover:drop-shadow-md dark:text-zinc-100 dark:hover:text-red-200 hover:cursor-pointer uppercase scale-y-90 tracking-wide font-thin text-xl hover:dark:bg-slate-700 hover:bg-zinc-300 rounded-lg">
+            <Link to={`${page.link}`} >
               {page.name}
-            {/* </Link> */}
+            </Link> 
           </div>
         ))} 
       </div>
@@ -34,7 +33,7 @@ function PageLinks() {
         </button>
         <div>
         {Object.values(pages).map((page) => (
-          <div className="text-left lg:blocks px-5 text-zinc-600 hover:text-red-400 hover:drop-shadow-md dark:text-zinc-100 dark:hover:text-red-200 hover:cursor-pointer uppercase scale-y-90 tracking-wide font-thin text-xl hover:dark:bg-stone-700">
+          <div className="text-left lg:blocks px-5 text-zinc-600 hover:text-red-400 hover:drop-shadow-md dark:text-zinc-100 dark:hover:text-red-200 hover:cursor-pointer uppercase scale-y-90 tracking-wide font-thin text-xl hover:dark:bg-stone-700 hover:bg-zinc-300">
             {/* <Link to={`/${page.name}`}> */}
 
               {page.name}
