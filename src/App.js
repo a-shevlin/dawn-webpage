c611648176dcc954df30af9bb8fdba5cf3399f2a
a-shev <alexshevlin1@gmail.com>
2022-12-29 17:50:41 -0800

    connect Contact route.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TitleCard, Home, PageLinks, About } from './components';
+import { TitleCard, Home, PageLinks, About, Contact } from './components';
 import {
   BrowserRouter as Router,
   Routes,
@@ -10,13 +10,14 @@ import {
 
 function App() {
   return (
-      <div className="h-screen bg-zinc-200 dark:bg-slate-800">
+      <div className="h-screen bg-zinc-200 dark:bg-gray-800">
     <Router>
         <TitleCard />
         <PageLinks />
         <Routes>
           <Route exact path='/' element={<Home />} />
           <Route path='/About' element={<About />} />
+          <Route path='/Contact' element={<Contact />} />
           {/* <Route path='/about' element={<AboutUs />} /> */}
         </Routes>
         {/* <div className="flex-1 w-full z-10 h-full">
