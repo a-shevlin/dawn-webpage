09bf637490baa4bba8cad2a36b21b3d3588cca58
a-shev <alexshevlin1@gmail.com>
2023-01-06 16:04:15 -0800

    change proptypes to include updated info

--- src/components/AllTiles.js
+++ src/components/AllTiles.js
@@ -1,21 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { Tiles } from "./";
-import { tiles } from "../data"; 
 
 
-function AllTiles() {
+function AllTiles(props) {
   return(
     <React.Fragment>
       <div className='w-full h-full flex flex-col bg-zinc-200 dark:bg-gray-800 p-1'>
         <div className='text-zinc-500 font-mono mx-auto w-full tracking-narrow drop-shadow-lg font-bold p-0 text-center uppercase grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4'>
 
-          {Object.values(tiles).map((tile) =>(
+          {Object.values(props.tileList).map((tile) =>(
             <Tiles
               img={tile.img}
-              name={tile.name}
+              title={tile.title}
               builder={tile.builder}
+              desc={tile.desc}
               id={tile.id}
+              key={tile.id}
             />
             ))}
         </div>
@@ -25,3 +26,7 @@ function AllTiles() {
 }
 
 export default AllTiles;
+
+AllTiles.propTypes = {
+  tileList: PropTypes.array,
+}
