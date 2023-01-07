50032581d1078643be0c58e9e5ecf896d08bee95
a-shev <alexshevlin1@gmail.com>
2023-01-06 16:04:45 -0800

    update proptypes to pass tileList

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { AllTiles } from "./";
 
-function Home() {
+function Home(props) {
+
   return(
     <React.Fragment>
       <div className="h-screen dark:bg-slate-800">
-        <AllTiles />
+        <AllTiles 
+          tileList = {props.tileList}
+        />
       </div>
     </React.Fragment>
   )
 }
 
 export default Home;
+
+Home.propTypes = {
+  tileList : PropTypes.array,
+}
