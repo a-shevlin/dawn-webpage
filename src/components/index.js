f5f441d3dd216f27b32da6653ef8facba84de327
a-shev <alexshevlin1@gmail.com>
2023-01-06 16:06:07 -0800

    add index export for TileDetails.

--- src/components/index.js
+++ src/components/index.js
@@ -6,3 +6,4 @@ export { default as AllTiles } from './AllTiles';
 export { default as PageLinks } from './PageLinks';
 export { default as About } from './About';
 export { default as Contact } from './Contact';
+export { default as TileDetails } from './TileDetails';
