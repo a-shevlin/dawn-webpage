e73b885be0f0c454737294d8c7419cf266cafdfc
a-shev <alexshevlin1@gmail.com>
2023-01-06 16:06:47 -0800

    WIP/ERROR useEffect broken after firestore implement.

--- src/components/TitleCard.js
+++ src/components/TitleCard.js
@@ -3,27 +3,21 @@ import PropTypes from "prop-types";
 import dawnLogo from '../assets/logo_tra.png';
 
 function TitleCard(props) {
-  const [opacity, setOpacity] = useState(1);
-  const [hidden, setHidden] = useState(false);
   const [hover, setHover] = useState(false);
   const [click, setClick] = useState(false);
   const headerRef = React.createRef();
 
+  const [opacity, setOpacity] = useState(1);
+  const [hidden, setHidden] = useState(false);
+
   // const location = useLocation()
 
-  const onHover = () => {
-    setHover(!hover)
-  }
-
-  const onLinkClick = () => {
-    setClick(!click);
-  }
-  
   useEffect(() => {  
-
-    // might need to change based on screen size
+    console.log(window.scrollY + " window scroll Y")
     
+    // might need to change based on screen size
     window.onscroll =()=>{
+      console.log('test scroll')
       const headerHeight = window.innerHeight;
       const range = 450;
       const offset = headerHeight / 2;
@@ -35,7 +29,7 @@ function TitleCard(props) {
       }
       
       setOpacity(calc);
-      console.log(calc);
+      console.log(calc + " calc");
 
       
       const height = window.innerHeight / 3;
@@ -47,6 +41,17 @@ function TitleCard(props) {
     };
     
   }, []);
+
+
+  const onHover = () => {
+    setHover(!hover)
+  }
+
+  const onLinkClick = () => {
+    setClick(!click);
+  }
+  
+  
   
   const copyThing = () => {
     navigator.clipboard.writeText('mc.dawnserver.com');
@@ -57,35 +62,39 @@ function TitleCard(props) {
 
 
   return (
-    <div className={`w-full flex flex-col justify-center bg-cover sticky bg-[url(../assets/dawn-bg.png)] bg-no-repeat bg-center h-full justify-center select-none z-0`}>
-      <div className={`text-red-100 font-mono fixed mx-auto w-full my-auto drop-shadow-lg font-bold p-0 text-center uppercase ${hidden && 'hidden'}`} style={{opacity: opacity}}>
-        <img className="w-32 block mx-auto" src={dawnLogo} alt="dawn logo" />
-        <h1 className={`text-8xl tracking-wide text-white -mt-4 scale-y-90`}>
-          {
-            window.location.pathname === "/" ? "DAWN" : window.location.pathname.substring(1,window.location.pathname.length)
-          }
-        </h1>
-        <p className="text-2xl font-extralight tracking-wider">
-          {
-            window.location.pathname === "/" ? "we are designers for the dawn of your creation" : ''
-          }     
-        </p>
-        <p 
-          className="text-lg border-double border-4 border-white rounded-md w-fit mx-auto font-black p-1 mt-2 tracking-widest"
-          onClick={() =>  copyThing()}
-          style={{cursor: "pointer"}}
-          onMouseEnter={onHover}
-          onMouseLeave={onHover}
-          
-          >
-            mc.dawnserver.com
-        </p>
-        {
-          click && hover ? "copied!" : hover ? "click to copy!":  
-          ""  
-        } 
+    <React.Fragment>
+      <div className={`w-full flex flex-col justify-center bg-cover sticky bg-[url(../assets/dawn-bg.png)] bg-no-repeat bg-center h-full z-0`}>
+        <div className='w-2/3 flex justify-center items-center m-auto h-full'>
+
+          <div className={`text-red-100 fixed font-mono mx-auto my-auto drop-shadow-lg font-bold p-0 text-center uppercase ${hidden && 'hidden'}`} style={{opacity: opacity}}>
+            <img className="w-32 block mx-auto" src={dawnLogo} alt="dawn logo" />
+            <h1 className={`text-8xl tracking-wide text-white -mt-4 scale-y-90`}>
+              {
+                window.location.pathname === "/" ? "DAWN" : window.location.pathname.substring(1,window.location.pathname.length)
+              }
+            </h1>
+            <p className="text-2xl font-extralight tracking-wider">
+              {
+                window.location.pathname === "/" ? "we are designers for the dawn of your creation" : ''
+              }     
+            </p>
+            <p 
+              className="text-lg border-double border-4 border-white rounded-md w-fit mx-auto font-black p-1 mt-2 tracking-widest"
+              onClick={() =>  copyThing()}
+              style={{cursor: "pointer"}}
+              onMouseEnter={onHover}
+              onMouseLeave={onHover}
+              >
+                mc.dawnserver.com
+            </p>
+            {
+              click && hover ? "copied!" : hover ? "click to copy!":  
+              ""  
+            } 
+          </div>
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   )
 }
 
@@ -95,5 +104,5 @@ TitleCard.propTypes = {
   logo: PropTypes.string,
   title: PropTypes.string,
   text: PropTypes.string,
-  id: PropTypes.number,
+  id: PropTypes.number
 }
