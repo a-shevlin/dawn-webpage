7c923d95dbd4ee306ef0e8bbe1d1fb2223e23eae
a-shev <alexshevlin1@gmail.com>
2023-01-08 01:55:28 -0800

    fix mobile styling and word wrap.

--- src/components/About.js
+++ src/components/About.js
@@ -7,19 +7,19 @@ function About() {
   
   return(
     <React.Fragment>
-      <div className="w-full mx-auto p-10 bg-stone-200 dark:bg-slate-800 dark:text-zinc-300">
+      <div className="w-full mx-auto py-10 bg-stone-200 dark:bg-slate-800 dark:text-zinc-300">
         <img src={logo} alt="image of dawn servers logo" className="w-90 block mx-auto"/>
         <p className="text-center text-xl">Dawn consists of over 15 talented builders, designers, developers, writers, and event planners. We use our expertise to create enjoyable minecraft experiences and content with the intention of enspiring happiness for people who use or view our work.</p>
         <div className="mx-auto w-full grid grid-cols-1 md:grid-cols-2 gap-12 py-5 uppercase">
           {Object.values(emp).map((tile) =>(
-            <div className="flex w-[28rem] m-auto whitespace-pre">
+            <div className="flex w-80 md:w-[28rem] m-auto whitespace-pre">
               {tile.id % 2 === 0 || tile.id === 0 ? 
                 <div className="w-full grid grid-cols-2 gap-1">
                   <img src={tile.img} alt={`image of ${tile.name}`} className='w-32 h-28 m-auto inline-block text-wrap px-4 border-r-2 dark:border-slate-300'/>
                   <div className="text-left scale-x-90">
                   <p className="text-2xl">{tile.mc}</p>
                   <p>{tile.name}</p>
-                  <p className="text-sm">{tile.job}</p>
+                  <p className="text-sm whitespace-normal">{tile.job}</p>
                   </div>
                 </div>
               :
@@ -27,7 +27,7 @@ function About() {
                   <div className="text-right scale-x-90">
                     <p className="text-2xl">{tile.mc}</p>
                     <p>{tile.name}</p>
-                    <p className="text-sm">{tile.job}</p>
+                    <p className="text-sm whitespace-normal">{tile.job}</p>
                   </div>
                   <img src={tile.img} alt={`image of ${tile.name}`} className='w-32 h-28 m-auto inline-block text-wrap px-4 border-l-2 dark:border-slate-300'/>
                 </div>
