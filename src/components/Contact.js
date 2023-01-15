48e7146437fe0fed887b8d2868f8ce29d0d84b81
a-shev <alexshevlin1@gmail.com>
2023-01-14 19:56:58 -0800

    fix styles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -24,19 +24,19 @@ function Contact() {
         {/* <div>
           <CategoryList />
         </div> */}
-        <div className='flex flex-col w-full mx-auto h-screen p-10 dark:bg-slate-800 bg-stone-200 text-gray-200'>
+        <div className='flex flex-col w-full mx-auto h-screen p-10 dark:bg-gray-800 bg-stone-200 text-gray-200'>
           <div className='mb-4 text-center mx-auto'>
             <h1 className="font-bold text-4xl p-2">Contact</h1>
             <p className='p-2'>Want to work with us? Fill out the form underneath and we will get back to you as soon as possible.</p>
           </div>
-          <form ref={form} onSubmit={sendEmail} className="flex-col w-72 mx-auto rounded-md">
+          <form ref={form} onSubmit={sendEmail} className="flex-col w-96 mx-auto rounded-md">
             <input type="text" name="from_name" placeholder="Name *" required className='mb-3 bg-gray-50 border text-gray-900 text-sm focus:ring-red-400 focus:border-red-400 block p-2.5 w-full border-gray-600 placeholder-gray-400 mx-auto rounded-md'/>
             <input type="email" name="from_email" placeholder="Email *" required className='mb-3 bg-gray-50 border text-gray-900 text-sm focus:ring-red-400 focus:border-red-400 block w-full p-2.5  border-gray-600 placeholder-gray-400 rounded-md' />
             <input type="text" name="discord" placeholder="Discord *" required className='mb-3 bg-gray-50 border text-gray-900 text-sm focus:ring-red-400 focus:border-red-400 block w-full p-2.5  border-gray-600 placeholder-gray-400 rounded-md' />
             <input type="text" name="budget" placeholder="Budget *" required className='mb-3 bg-gray-50 border text-gray-900 text-sm focus:ring-red-400 focus:border-red-400 block w-full p-2.5  border-gray-600 placeholder-gray-400 rounded-md' />
             <input type="text" name="size" placeholder="Size *" required className='mb-3 bg-gray-50 border text-gray-900 text-sm focus:ring-red-400 focus:border-red-400 block w-full p-2.5  border-gray-600 placeholder-gray-400 rounded-md' />
             <textarea type="text" name="details" placeholder="Details *" required className='mb-6 bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block h-2/4 w-full p-2.5  dark:border-gray-600 rounded-md'/>
-            <button type="submit" value="Send" className='border cursor-pointer dark:border-gray-700 text-gray-600 font-black transition duration-500 ease-in-out delay-550 hover:dark:border-red-200 hover:text-red-200 font-bold py-2 px-6 mr-4 rounded-lg header-title float-right'> Send </button>
+            <button type="submit" value="Send" className='border cursor-pointer dark:border-gray-700 text-gray-600 transition duration-500 ease-in-out delay-550 hover:dark:border-red-200 hover:text-red-200 font-bold py-2 px-6 mr-4 rounded-lg header-title float-right'> Send </button>
           </form>
         </div>
       </div>
