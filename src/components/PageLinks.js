51177b9299867fe1f89e2bd807478c1ce50bd6d6
a-shev <alexshevlin1@gmail.com>
2023-01-31 01:12:00 -0800

    add dropdown on click and alternate colors for mobile.

--- src/components/PageLinks.js
+++ src/components/PageLinks.js
@@ -4,10 +4,11 @@ import { pages }from "../data";
 
 function PageLinks() {
   const [width, setWidth] = useState(false);
+  const [navOpen, setNavOpen] = useState(false);
 
   useEffect(() => {
     window.onresize = () => {
-      (window.innerWidth < 950) ? setWidth(true) : setWidth(false);
+      (window.innerWidth < 1000) ? setWidth(true) : setWidth(false);
     }
   }, []);
   return(
@@ -25,19 +26,28 @@ function PageLinks() {
         ))} 
       </div>
     : 
-      <div>
-        <button className="text-right">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-8 h-6 dark:text-zinc-200 stroke-current rounded-lg dark:ahover:bg-gray-700">
+      <div className="w-2/3">
+        <button className="text-right" onClick={() => setNavOpen(!navOpen)} >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-24 h-16 dark:text-zinc-200 stroke-current rounded-lg ">
             <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
           </svg>
         </button>
         <div>
         {Object.values(pages).map((page) => (
-          <div className="text-left lg:blocks px-5 text-zinc-600 hover:text-red-400 rounded hover:drop-shadow-md dark:text-zinc-100 dark:hover:text-red-200 hover:cursor-pointer uppercase scale-y-90 tracking-wide font-thin text-xl hover:dark:bg-gray-700 hover:bg-zinc-300">
-            {/* <Link to={`/${page.name}`}> */}
-
-              {page.name}
-            {/* </Link> */}
+          <div className={"text-center lg:blocks text-zinc-600 hover:text-red-400 rounded hover:drop-shadow-md dark:text-zinc-100 dark:hover:text-red-200 hover:cursor-pointer uppercase scale-y-90 tracking-wide font-thin text-2xl" + (navOpen ? " h-12 " : " h-0 ") + " transition-all delay-150 duration-300 overflow-hidden "} >
+            {page.id%2 === 0 ?
+              <li className="w-full py-[0.5rem] px-4 bg-[#e2d8d8] hover:bg-[#dccaca] dark:bg-[#19212c] hover:dark:bg-[#1f2734] rounded-lg list-none">
+                <Link to={`/${page.name}`}>
+                  {page.name}
+                </Link>
+              </li>
+            :
+              <li className="w-full py-[0.5rem] px-4 hover:dark:bg-[#293343] hover:bg-[#eee2e2] rounded-lg list-none">
+                <Link to={`/${page.name}`} className="">
+                  {page.name}
+                </Link>
+              </li>
+            }
           </div>
         ))}
         </div>
